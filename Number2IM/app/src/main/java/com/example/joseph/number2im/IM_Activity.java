5dc3ed07e95be5fc27554cf87cf5c0a79782de56
Joseph Buchoff <joseph.buchoff@rutgers.edu>
2016-02-06 15:59:12 -0500

    Added Android App

---
+++ Number2IM/app/src/main/java/com/example/joseph/number2im/IM_Activity.java
@@ -0,0 +1,13 @@
+package com.example.joseph.number2im;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IM_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_im_);
+    }
+}
